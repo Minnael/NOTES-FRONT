a7894662429360685134cd36f96b55fb820e0b8c
Minnael <minnaelcampelo@gmail.com>
2024-02-08 08:52:52 -0300

    desenvolvimento do modal cards

--- src/Components/new-note-card.tsx
+++ src/Components/new-note-card.tsx
@@ -1,13 +1,76 @@
+import * as Diolog from '@radix-ui/react-dialog'
+import {X} from 'lucide-react'
+import {ChangeEvent, FormEvent, useState} from 'react'
+import {toast} from 'sonner'
+
 function NewNoteCard(){
+   const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true)
+   const [content, setContent] = useState('')
+
+
+   function handleStartEditor(){
+      setShouldShowOnboarding(false)
+   }
+
+   function handleContentChanged(event: ChangeEvent<HTMLTextAreaElement>){
+      setContent(event.target.value)
+
+      if (event.target.value === ''){
+         setShouldShowOnboarding(true)
+      }
+   }
+
+   function handleSaveNote(event: FormEvent){
+      event.preventDefault()
+      console.log(content)
+      toast.success('Nota criada com sucesso!')
+   }
+
    return (
-      <div className="rounded-md bg-slate-700 p-5 space-y-3">
-         <span className="text-sm font-medium text-slate-200">
-            Adicionar nota
-         </span>
-         <p className="text-sm leading-6 text-slate-400">
-            Grave uma nota em áudio que será convertida para texto automaticamente.
-         </p>
-      </div>
+      <Diolog.Root>
+         <Diolog.Trigger className="flex flex-col rounded-md bg-slate-700 text-left p-5 gap-3 hover:ring-2 hover:ring-slate-600 outline-none focus-visible:ring-2 focus-visible:ring-lime-400">
+            <span className="text-sm font-medium text-slate-200">
+               Adicionar nota
+            </span>
+            <p className="text-sm leading-6 text-slate-400">
+               Grave uma nota em áudio que será convertida para texto automaticamente.
+            </p>
+         </Diolog.Trigger>
+
+         <Diolog.Portal>
+            <Diolog.Overlay className="inset-0 fixed bg-black/60"/>
+            <Diolog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 outline-none -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col">
+               <Diolog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+                  <X className="size-5"/>
+               </Diolog.Close>
+               
+               <form onSubmit={handleSaveNote} className="flex-1 flex flex-col">
+                  <div className="flex flex-1 flex-col gap-3 p-5">
+                     <span className="text-sm font-medium text-slate-300">
+                        Adicionar nota
+                     </span>
+
+                     {shouldShowOnboarding ? (
+                        <p className="text-sm leading-6 text-slate-400">
+                           Comece <button className="font-medium text-lime-400 hover:underline">gravando uma nota</button> em áudio ou se preferir <button onClick={handleStartEditor} className="font-medium text-lime-400 hover:underline">utilize apenas texto</button>.
+                        </p>
+                     ) : (
+                        <textarea
+                           autoFocus 
+                           className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
+                           onChange={handleContentChanged}
+                        />
+                     )}
+                  </div>
+
+                  <button type="submit" className="w-full bg-lime-400 py-4 text-center text-lime-950 text-sm outline-none font-medium hover:bg-lime-500">
+                     Salvar nota
+                  </button>
+               </form>
+            </Diolog.Content>
+         </Diolog.Portal>
+
+      </Diolog.Root>
    )
 }
 
--- src/Components/note-card.tsx
+++ src/Components/note-card.tsx
@@ -1,15 +1,51 @@
-function NoteCard(){
-   return (
-      <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 outline-none focus-visible:ring-2 focus-visible:ring-lime-400">
-         <span className="text-sm font-medium text-slate-300">
-            há 2 dias
-         </span>
-         <p className="text-sm leading-6 text-slate-400">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores voluptas recusandae enim dolore culpa laborum sequi, sunt eligendi soluta molestias at ad nesciunt aliquam nemo harum expedita? Id, quae ratione!
-         </p>
+import * as Diolog from '@radix-ui/react-dialog'
+import {formatDistanceToNow} from 'date-fns'
+import {ptBR} from 'date-fns/locale'
+import {X} from 'lucide-react'
 
-          <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none"/>
-      </button>
+interface NoteCardProps {
+   note: {
+      date: Date
+      content: string
+   }
+}
+
+function NoteCard({note}: NoteCardProps){
+   return (
+      <Diolog.Root>
+         <Diolog.Trigger className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 outline-none focus-visible:ring-2 focus-visible:ring-lime-400">
+            <span className="text-sm font-medium text-slate-300">
+               {note.date.toISOString()}
+            </span>
+            <p className="text-sm leading-6 text-slate-400">
+               {note.content}
+            </p>
+
+            <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none"/>
+         </Diolog.Trigger>
+
+         <Diolog.Portal>
+            <Diolog.Overlay className="inset-0 fixed bg-black/60"/>
+            <Diolog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 outline-none -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col">
+               <Diolog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+                  <X className="size-5"/>
+               </Diolog.Close>
+               
+               <div className="flex flex-1 flex-col gap-3 p-5">
+                  <span className="text-sm font-medium text-slate-300">
+                     {formatDistanceToNow(note.date, {locale: ptBR, addSuffix: true})}
+                  </span>
+                  <p className="text-sm leading-6 text-slate-400">
+                     {note.content}
+                  </p>
+               </div>
+
+               <button type="button" className="w-full group bg-slate-800 py-4 text-center text-slate-300 text-sm outline-none font-medium">
+                  Deseja <span className="text-red-400 group-hover:underline">apagar essa nota</span>?
+               </button>
+            </Diolog.Content>
+         </Diolog.Portal>
+      </Diolog.Root>
    )
 }
 
